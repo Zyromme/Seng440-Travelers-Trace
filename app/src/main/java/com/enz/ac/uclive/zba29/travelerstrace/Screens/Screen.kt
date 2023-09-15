8eee9beb05ada6e0f690506b72252c27d4c4ed17
Zyromme Bayson <zba29@uclive.ac.nz>
2023-09-15 17:07:14 +1200

    Added Navigation

---
+++ app/src/main/java/com/enz/ac/uclive/zba29/travelerstrace/Screens/Screen.kt
@@ -0,0 +1,17 @@
+package com.enz.ac.uclive.zba29.travelerstrace.Screens
+
+sealed class Screen(val route: String) {
+    object MainScreen : Screen("main_screen")
+    object MapScreen : Screen("map_screen")
+    object JourneyDetailScreen : Screen("journey_detail_screen")
+    object SettingsScreen : Screen("settings_screen")
+
+    fun withArgs(vararg args: String): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
+    }
+}
