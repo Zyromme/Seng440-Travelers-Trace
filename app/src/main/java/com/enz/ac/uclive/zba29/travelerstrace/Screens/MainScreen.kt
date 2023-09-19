61af7a43569ad21e41b2da9e6125a5a3a6ac2591
Zyromme Bayson <zba29@uclive.ac.nz>
2023-09-19 14:06:53 +1200

    Changed some colours

--- app/src/main/java/com/enz/ac/uclive/zba29/travelerstrace/Screens/MainScreen.kt
+++ app/src/main/java/com/enz/ac/uclive/zba29/travelerstrace/Screens/MainScreen.kt
@@ -4,16 +4,13 @@ import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
