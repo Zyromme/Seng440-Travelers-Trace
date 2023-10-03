334ab4e2a9b0ad386a73f4cf554acabb367eb87b
Arish Abalos <ama541@uclive.ac.nz>
2023-10-04 01:38:34 +1300

    Changed icons to be loaded using image_vectors

--- app/src/main/java/com/enz/ac/uclive/zba29/travelerstrace/component/Drawer.kt
+++ app/src/main/java/com/enz/ac/uclive/zba29/travelerstrace/component/Drawer.kt
@@ -24,23 +24,22 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.enz.ac.uclive.zba29.travelerstrace.R
-import com.enz.ac.uclive.zba29.travelerstrace.model.AppDrawerItemInfo
+import com.enz.ac.uclive.zba29.travelerstrace.model.DrawerParams
 import kotlinx.coroutines.launch
 
 
 @Composable
 fun Drawer(
     drawerState: DrawerState,
-    menuItems: List<AppDrawerItemInfo>,
     navController: NavController,
 ) {
+    val menuItems = DrawerParams.drawerButtons
     val scope = rememberCoroutineScope()
     var selectedItemIndex by rememberSaveable {
          mutableIntStateOf(0)
@@ -95,7 +94,7 @@ fun Drawer(
                               },
                     icon = {
                     Icon(
-                        painter = painterResource(id = drawerItem.drawableId),
+                        imageVector = drawerItem.icon,
                         contentDescription = stringResource(id = drawerItem.title)
                     )
                 }, modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
