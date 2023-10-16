3eb354579ca6ec5bd1138f7818a9941daa8dbd3c
Arish Abalos <ama541@uclive.ac.nz>
2023-10-16 15:26:24 +1300

    Code to optimize location was done on a separate computer but same time as previous commit, forgot to push...

--- app/src/main/java/com/enz/ac/uclive/zba29/travelerstrace/model/Journey.kt
+++ app/src/main/java/com/enz/ac/uclive/zba29/travelerstrace/model/Journey.kt
@@ -1,5 +1,7 @@
 package com.enz.ac.uclive.zba29.travelerstrace.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -13,5 +15,41 @@ class Journey(
     @ColumnInfo var totalDistance: Double,
     @ColumnInfo var type: String,
     @ColumnInfo var duration: Int,
+    ) : Parcelable {
+
+    // Constructor for parcelable
+    constructor(parcel: Parcel) : this(
+        parcel.readLong(),
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readDouble(),
+        parcel.readString() ?: "",
+        parcel.readInt()
     )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeLong(id)
+        parcel.writeString(title)
+        parcel.writeString(description)
+        parcel.writeString(date)
+        parcel.writeDouble(totalDistance)
+        parcel.writeString(type)
+        parcel.writeInt(duration)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Journey> {
+        override fun createFromParcel(parcel: Parcel): Journey {
+            return Journey(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Journey?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
 
